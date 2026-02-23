e0fa660be6dfee731c922d1e9286ad12b5429842
Migue-debug-coder <migueldiazromero4@gmail.com>
2026-02-23 10:20:18 +0100

    Actualización del Proyecto

--- src/main/java/ies_alcores/api_productos/repository/CategoriaRepository.java
+++ src/main/java/ies_alcores/api_productos/repository/CategoriaRepository.java
@@ -3,9 +3,14 @@ package ies_alcores.api_productos.repository;
 import ies_alcores.api_productos.model.Categoria;
 import ies_alcores.api_productos.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, String> {
-    Optional<Categoria> findById(long id);
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    @Query("select c from Categoria c where c.id between 1 and 5")
+    List<Categoria> findIdBetween1and5;
+
 }
